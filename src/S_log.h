b61331edb3b846b362041be9bf40d03d141cfc54
nonk123 <me@nonk.dev>
2025-07-24 17:17:31 +0300

    That's not how you log!!!

--- src/S_log.h
+++ src/S_log.h
@@ -2,6 +2,11 @@
 
 #include <stdio.h>
 
-#define INFO(fmt, ...)                                                                                                 \
-    printf(fmt "\n", ##__VA_ARGS__);                                                                                   \
-    fflush(stdout);
+#define _LOG(level, file, line, fmt, ...)                                                                              \
+    do {                                                                                                               \
+        printf("%s [%s:%d]: " fmt "\n", level, file, line, ##__VA_ARGS__);                                             \
+        fflush(stdout);                                                                                                \
+    } while (0)
+
+#define LOG(level, ...) _LOG(level, __FILE__, __LINE__, __VA_ARGS__)
+#define INFO(...) LOG("INFO", __VA_ARGS__)
